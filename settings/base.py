2b85e417e505937d4e475d16e6eb9203ea38fbe4
Dale Floer <dalefloer@gmail.com>
2017-04-25 20:00:51 -0700

    Changed to not use timezone aware datetime to supress warning.

--- settings/base.py
+++ settings/base.py
@@ -114,7 +114,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False  # timestamps in the source ebird data are naive, so we're using naive timezones for now.
 
 
 # Static files (CSS, JavaScript, Images)
