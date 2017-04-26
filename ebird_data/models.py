8f5b98731dd9b8f38b5b15cd4c6c1c9019b3a5e6
Dale Floer <dalefloer@gmail.com>
2017-04-25 20:00:08 -0700

    Updated the models and importer to better reflect the data.

--- ebird_data/models.py
+++ ebird_data/models.py
@@ -2,16 +2,16 @@ from django.contrib.gis.db import models
 
 
 class Observation(models.Model):
-    checklist_id = models.ForeignKey('Checklist')
+    checklist = models.ForeignKey('Checklist')
     observation_id = models.IntegerField(primary_key=True)
-    number_observed = models.IntegerField(null=True)
+    number_observed = models.IntegerField(null=True, blank=True)
     is_x = models.BooleanField()
     age_sex = models.TextField()  # This is a first pass, this field seems like it's freeform text of structured data.
-    species_comments = models.TextField()
+    species_comments = models.TextField(null=True, blank=True)
     species = models.ForeignKey('Species')
     subspecies = models.ForeignKey('SubSpecies')
     breeding_atlas_code = models.ForeignKey('BreedingAtlas')
-    date_last_edit = models.DateTimeField()
+    date_last_edit = models.DateTimeField(null=True, blank=True)
     has_media = models.BooleanField()
     bcr_code = models.ForeignKey('BCR')
     usfws_code = models.ForeignKey('USFWS')
@@ -29,15 +29,15 @@ class BreedingAtlas(models.Model):
 
 class Checklist(models.Model):
     location = models.ForeignKey('Location')
-    start_date_time = models.DateTimeField()
+    start_date_time = models.DateTimeField(null=True, blank=True)
     checklist_comments = models.TextField()
-    checklist_id = models.IntegerField(primary_key=True)
-    duration = models.DurationField()
-    distance = models.DecimalField(decimal_places=6, max_digits=16)
-    area = models.DecimalField(decimal_places=6, max_digits=16)
-    number_of_observers = models.IntegerField()
+    checklist = models.IntegerField(primary_key=True)
+    duration = models.DurationField(null=True, blank=True)
+    distance = models.DecimalField(decimal_places=6, max_digits=16, null=True, blank=True)
+    area = models.DecimalField(decimal_places=6, max_digits=16, null=True, blank=True)
+    number_of_observers = models.IntegerField(null=True, blank=True)
     complete_checklist = models.BooleanField()
-    group_id = models.IntegerField(blank=True)  # Should this be a fk to a seperate model?
+    group_id = models.IntegerField(null=True, blank=True)  # Should this be a fk to a seperate model?
     approved = models.BooleanField()
     reviewed = models.BooleanField()
     reason = models.TextField()
@@ -54,8 +54,8 @@ class Project(models.Model):
 
     
 class Species(models.Model):
-    common_name = models.TextField(primary_key=True)
-    scientific_name = models.TextField(unique=True)
+    common_name = models.TextField()
+    scientific_name = models.TextField(primary_key=True)
     taxonomic_order = models.ForeignKey('TaxonomicOrder')
     category = models.ForeignKey('SpeciesCategory')
     
@@ -76,7 +76,7 @@ class SpeciesCategory(models.Model):
     
 class Location(models.Model):
     coords = models.PointField(srid=4326)
-    locality_id = models.ForeignKey('Locality')
+    locality = models.ForeignKey('Locality')
     country = models.ForeignKey('Country')
     state_province = models.ForeignKey('StateProvince')
     county = models.ForeignKey('County')
@@ -85,7 +85,6 @@ class Location(models.Model):
 
 class Locality(models.Model):
     locality_id = models.IntegerField(primary_key=True)
-    category = models.TextField()
     locality_type = models.CharField(max_length=2)
 
 
@@ -111,5 +110,5 @@ class IBA(models.Model):
 class BCR(models.Model):
     bcr_code = models.TextField(primary_key=True)
     
-class BCR(models.Model):
+class USFWS(models.Model):
     usfws_code = models.TextField(primary_key=True)
--- ebird_data_import.py
+++ ebird_data_import.py
@@ -39,107 +39,171 @@ def parse_ebird_dump(file_path):
         now_format = "%d/%m/%y %H:%M:%S"
         print("Start time:", datetime.strftime(now, now_format))
         for e in reader:
-            # Observation
-            # ID in the data has the form of URN:CornellLabOfOrnithology:EBIRD:OBS######, and we want just the #s at the end for the id.
-            observation_id = int(e['GLOBAL UNIQUE IDENTIFIER'].split(':')[-1][3:])
-            # ID in the data has form 'S########' and we want just the #s at the end.
-            checklist_id = int(e['SAMPLING EVENT IDENTIFIER'][1:])
-            count = e['OBSERVATION COUNT']
-            if count == 'X':
-                number_observed = None
-                is_x = True
-            else:
-                number_observed = count
-                is_x = False
-            age_sex = e['AGE/SEX']
-            species_comments = e['SPECIES COMMENTS']
-            breeding_atlas_code = e['BREEDING BIRD ATLAS CODE']        
-            # Species
-            taxonomic_order = e['TAXONOMIC ORDER']
-            species_category = e['CATEGORY']
-            commone_name = e['COMMON NAME']
-            scientific_name = e['SCIENTIFIC NAME']
-            subspecies_common_name = e['SUBSPECIES COMMON NAME']
-            subspecies_scientific_name = e['SUBSPECIES SCIENTIFIC NAME']
-            #Checklist
-            checklist_date = e['OBSERVATION DATE']
-            month, day, year = [int(x) for x in re.split('[/\-]' ,checklist_date)]
-            checklist_time = e['TIME OBSERVATIONS STARTED']
-            #if checklist_time == '':
-            #else:
-            print(e)
-            hour, minute, second = [int(x) for x in checklist_time.split(':')]
-            start = datetime.datetime(year, month, day, hour, minute, second, 0)
-            checklist_comments = e['TRIP COMMENTS']
-            checklist_duration = int(e['DURATION MINUTES'])
-            end = datetime.datetime(year, month, day, hour, minute + checklist_duration, second, 0)
-            duation = end - start
-            distance = Decimal(e['EFFORT DISTANCE KM'])
-            area = Decimal(e['EFFORT AREA HA'])
-            number_of_observers = int(e['NUMBER OBSERVERS'])
-            complete_checklist = bool(int(e['ALL SPECIES REPORTED']))
-            # group id in the form of 'G######' but we want just #s.
-            group_id = int(e['GROUP IDENTIFIER'][1:])
-            approved = bool(int(e['APPROVED']))
-            reviewed = bool(int(e['REVIEWED']))
-            reason = e['REASON']
-            protocol = e['PROTOCOL TYPE']
-            # Project
-            project = e['PROJECT CODE']
-            # Observer
-            # The is is in the form of 'obsr######' but we want just the #s.
-            observer_id = int(e['OBSERVER ID'][4:])
-            first_name = e['FIRST NAME']
-            last_name = e['LAST NAME']
-            # Location
-            lat = float(e['LATITUDE'])
-            lon = float(e['LONGITUDE'])
-            coords = Point(lon, lat)  # PostGIS and GeoDjango both expect this as a longitude/latitude pair.
-            locality = e['LOCALITY']
-            # In data in the form of 'L#######' but we want just #s.
-            localist_id = int(e['LOCALITY ID'][1:])
-            locality_type = e['LOCALITY TYPE']
-            subnational_1 = e['SUBNATIONAL1_CODE']
-            subnational_2 = e['SUBNATIONAL2_CODE']
-            county = e['COUNTY']
-            iba_code = e['IBA CODE']
-            state_province = e['STATE_PROVINCE']
-            country = e['COUNTRY']
-            country_code = e['COUNTRY_CODE']
+            try:    
+                # Observation
+                # ID in the data has the form of URN:CornellLabOfOrnithology:EBIRD:OBS######, and we want just the #s at the end for the id.
+                observation_id = int(e['GLOBAL UNIQUE IDENTIFIER'].split(':')[-1][3:])
+                # ID in the data has form 'S########' and we want just the #s at the end.
+                checklist_id = int(e['SAMPLING EVENT IDENTIFIER'][1:])
+                obs_count = e['OBSERVATION COUNT']
+                if obs_count == 'X':
+                    number_observed = None
+                    is_x = True
+                else:
+                    number_observed = count
+                    is_x = False
+                age_sex = e['AGE/SEX']
+                species_comments = e['SPECIES COMMENTS']
+                breeding_atlas_code = e['BREEDING BIRD ATLAS CODE']        
+                # Species
+                taxonomic_order = e['TAXONOMIC ORDER']
+                species_category = e['CATEGORY']
+                common_name = e['COMMON NAME']
+                scientific_name = e['SCIENTIFIC NAME']
+                subspecies_common_name = e['SUBSPECIES COMMON NAME']
+                subspecies_scientific_name = e['SUBSPECIES SCIENTIFIC NAME']
+                #Checklist
+                checklist_date = e['OBSERVATION DATE']
+                checklist_time = e['TIME OBSERVATIONS STARTED']
+                checklist_duration = e['DURATION MINUTES']
+                start, duration = parse_start_duration(checklist_date, checklist_time, checklist_duration)
+                checklist_comments = e['TRIP COMMENTS']
+                #distance = Decimal(e['EFFORT DISTANCE KM'])
+                #area = Decimal(e['EFFORT AREA HA'])
+                distance = decimal_or_none(e['EFFORT DISTANCE KM'])
+                area = decimal_or_none(e['EFFORT AREA HA'])
+                number_of_observers = int_or_none(e['NUMBER OBSERVERS'])
+                complete_checklist = bool(int(e['ALL SPECIES REPORTED']))
+                # group id in the form of 'G######' but we want just #s.
+                group_id = int_or_none(e['GROUP IDENTIFIER'][1:])
+                approved = bool(int(e['APPROVED']))
+                reviewed = bool(int(e['REVIEWED']))
+                reason = e['REASON']
+                protocol = e['PROTOCOL TYPE']
+                # Project
+                project = e['PROJECT CODE']
+                # Observer
+                # The is is in the form of 'obsr######' but we want just the #s.
+                observer_id = int(e['OBSERVER ID'][4:])
+                first_name = e['FIRST NAME']
+                last_name = e['LAST NAME']
+                # Location
+                lat = float(e['LATITUDE'])
+                lon = float(e['LONGITUDE'])
+                coords = Point(lon, lat)  # PostGIS and GeoDjango both expect this as a longitude/latitude pair.
+                locality = e['LOCALITY']
+                # In data in the form of 'L#######' but we want just #s.
+                locality_id = int(e['LOCALITY ID'][1:])
+                locality_type = e[' LOCALITY TYPE']  # No, that leading space isn't an error, in original data.
+                state_code = e['STATE CODE']
+                county_code = e['COUNTY CODE']
+                county = e['COUNTY']
+                iba_code = e['IBA CODE']
+                state_province = e['STATE']
+                country = e['COUNTRY']
+                country_code = e['COUNTRY CODE']
+                bcr_code = e['BCR CODE']
+                usfws_code = e['USFWS CODE']
+                has_media = bool(int(e['HAS MEDIA']))
+                edit = e['LAST EDITED DATE']
+                if edit != '':
+                    last_edit = datetime.strptime(edit, "%Y-%m-%d %H:%M:%S")
+                else:
+                    last_edit = None
+                # Start with the models that don't depend on other models and have single attributes.
+                iba, _ = IBA.objects.get_or_create(iba_code=iba_code)
+                tax, _ = TaxonomicOrder.objects.get_or_create(taxonomic_order=taxonomic_order)
+                spcat, _ = SpeciesCategory.objects.get_or_create(category=species_category)
+                proto, _ = Protocol.objects.get_or_create(protocol_type=protocol)
+                proj, _ = Project.objects.get_or_create(project_code=project)
+                atlas, _ = BreedingAtlas.objects.get_or_create(breeding_atlas_code=breeding_atlas_code)
+                bcr, _ = BCR.objects.get_or_create(bcr_code=bcr_code)
+                usfws, _ = USFWS.objects.get_or_create(usfws_code=usfws_code)
+                # Continue with models that don't depend on others, but that have multiple attributes.
+                sp, _ = StateProvince.objects.get_or_create(state_province=state_province, defaults={'state_code': state_code})
+                cnty, _ = County.objects.get_or_create(county=county, defaults={'county_code': county_code})
+                local, _ = Locality.objects.get_or_create(locality_id=locality_id, defaults={'locality_type': locality_type})
+                cntry, _ = Country.objects.get_or_create(country=country, defaults={'country_code': country_code})
+                obs, _ = Observer.objects.get_or_create(observer_id=observer_id, defaults={'first_name': first_name, 'last_name': last_name})
+                # Then continue with the models that only depend on the ones we've got.
+                loc, _ = Location.objects.get_or_create(coords=coords, defaults={'locality': local, 'country': cntry, 'state_province': sp, 'county': cnty, 'iba_code': iba})
+                sp, _ = Species.objects.get_or_create(scientific_name=scientific_name, defaults={'common_name': common_name, 'taxonomic_order': tax, 'category': spcat})
+                # SubSpecies depends on Species.
+                subsp, _ = SubSpecies.objects.get_or_create(scientific_name=subspecies_scientific_name, defaults={'parent_species': sp, 'common_name': subspecies_common_name})
+                # Next the checklist model
+                check, _ = Checklist.objects.get_or_create(checklist=checklist_id, defaults={'location': loc, 'start_date_time': start, 'checklist_comments': checklist_comments, 'duration': duration, 'distance': distance, 'area': area, 'number_of_observers': number_of_observers, 'complete_checklist': complete_checklist, 'group_id': group_id, 'approved': approved, 'reviewed': reviewed, 'reason': reason, 'protocol': proto, 'project': proj})
+                # Finally the remaining models that depend on all the previous ones.
+                obs = Observation(observation_id=observation_id, number_observed=number_observed, is_x=is_x, age_sex=age_sex, species_comments=species_comments, species=sp, subspecies=subsp, breeding_atlas_code=atlas, date_last_edit=last_edit, has_media=has_media, bcr_code=bcr, usfws_code=usfws, checklist=check)
+                obs.save()
             
-            # Start with the models that don't depend on other models and have single attributes.
-            sp, _ = StateProvince.objects.get_or_create(state_province)
-            sn1, _ = StateCode.objects.get_or_create(subnational_1)
-            sn2, _ = CountyCode.objects.get_or_create(subnational_2)
-            cnty, _ = County.objects.get_or_create(county)
-            iba, _ = IBA.objects.get_or_create(iba_code)
-            tax, _ = TaxonomicOrder.objects.get_or_create(taxonomic_order)
-            spcat, _ = SpeciesCategory.objects.get_or_create(species_category)
-            proto, _ = Protocol.objects.get_or_create(protocol)
-            proj, _ = Project.objects.get_or_create(project)
-            atlas, _ = BreedingAtlas.objects.get_or_create(breeding_atlas_code)
-            # Continue with models that don't depend on others, but that have multiple attributes.
-            local, _ = Locality.objects.get_or_create(locality_id, category, locality_type)
-            cntry, _ = Country.objects.get_or_create(country_code, country)
-            obs, _ = Observer.objects.get_or_create(observer_id, first_name, last_name)
-            # Then continue with the models that only depend on the ones we've got.
-            loc, _ = Location.objects.get_or_create(coords, local, cntry, sp, sn1, sn2, cnty, iba)
-            sp, _ = Species.objects.get_or_create(common_name, scientific_name, tax, spcat)
-            # SubSpecies depends on Species.
-            subsp, _ = SubSpecies.objects.get_or_create(sp, subspecies_common_name, subspecies_scientific_name)
-            # Next the checklist model
-            check, _ = Checklist.objects.get_or_create(loc, start, checklist_comments, checklist_id, duration, distance, area, observer_count, complete_checklist, group_id, approved, reviewed, reason, proto, proj)
-            # Finally the remaining models that depend on all the previous ones.
-            obs = Observation(check, observation_id, number_observed, is_x, age_sex, species_comments, sp, subsp, atlas)
-            obs.save()
-            
-            count += 1           
-            if count % 100000:
-                now = datetime.now()
-                now_format = "%d/%m/%y %H:%M:%S"
-                print(datetime.strftime(now, now_format), count)
-            print("Final count:", count, "End time:", datetime.strftime(now, now_format))
+                count += 1
+                if count % 1000 == 0:
+                    now = datetime.now()
+                    now_format = "%d/%m/%y %H:%M:%S"
+                    print(datetime.strftime(now, now_format), count)
+            except Exception as ex:
+                print(count)
+                print(e)
+                raise ex
         
+        now = datetime.now()
+        now_format = "%d/%m/%y %H:%M:%S"
+        print("Final count:", count, "End time:", datetime.strftime(now, now_format))
+
+def parse_start_duration(checklist_date, checklist_time, checklist_duration):
+    """
+    Parses a checklist's start date and time and duration in minutes.
+    Returns a datetime object for the start date and time and a timedelta for the duration
+    Will return None in cases where we don't have enough information to determine these.
+    """
+    # No time and no date in ebird data.
+    if checklist_duration == '':
+        duration = None
+    else:
+        duration_mins = int(checklist_duration)
+        duration_start = datetime(1900, 1, 1, 0, 0, 0, 0)
+        h = duration_mins // 60
+        m = duration_mins % 60
+        duration_end = datetime(1900, 1, 1, h, m, 0, 0)
+        duration = duration_end - duration_start
+    # empty date and time
+    if checklist_date == '' and checklist_time == '':
+        start = None
+    # Non-empty date but empty start time.
+    elif checklist_date != '' and checklist_time == '':
+        year, month, day = parse_date(checklist_date)
+        start = datetime(year, month, day, 0, 0, 0, 0)
+    else:
+        year, month, day = parse_date(checklist_date)
+        hour, minute, second = parse_time(checklist_time)
+        start = datetime(year, month, day, hour, minute, second, 0)
+    return start, duration
+            
+def parse_date(date_str):
+    """
+    Takes a string of the form mm-dd-yyyy (where the delimeter can be /, \ or - and returns year, month and day tuple.
+    """    
+    year, month, day = [int(x) for x in re.split('[/\-]', date_str)]
+    return year, month, day
+    
+def parse_time(time_str):
+    """
+    Takes a string of the form hh:mm:ss and returns (hours, minutes, seconds) tuple.
+    """
+    h, m, s = [int(x) for x in time_str.split(':')]
+    return h, m, s
+    
+def decimal_or_none(d):
+    if d == '':
+        return None
+    else:
+        return Decimal(d)
+        
+def int_or_none(i):
+    if i == '':
+        return None
+    else:
+        return int(i)
         
 def parse_command_line():
     parser = argparse.ArgumentParser()
@@ -151,6 +215,6 @@ def parse_command_line():
 if __name__ == "__main__":
     options = parse_command_line()
     input_file = options.input_file
-    #parse_ebird_dump(input_file)
-    open_tsv_header(input_file)
+    parse_ebird_dump(input_file)
+    #open_tsv_header(input_file)
     
