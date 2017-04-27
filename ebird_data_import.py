4df91a1750c9dd1d7e63287fa47d2c0fb8f09cb6
Dale Floer <dalefloer@gmail.com>
2017-04-26 18:56:35 -0700

    Parse now knows about nullable FKs.

--- ebird_data_import.py
+++ ebird_data_import.py
@@ -118,30 +118,61 @@ def parse_ebird_dump(file_path, start_row):
                     else:
                         last_edit = None
                     # Start with the models that don't depend on other models and have single attributes.
-                    iba, _ = IBA.objects.get_or_create(iba_code=iba_code)
-                    tax, _ = TaxonomicOrder.objects.get_or_create(taxonomic_order=taxonomic_order)
-                    spcat, _ = SpeciesCategory.objects.get_or_create(category=species_category)
-                    proto, _ = Protocol.objects.get_or_create(protocol_type=protocol)
-                    proj, _ = Project.objects.get_or_create(project_code=project)
-                    atlas, _ = BreedingAtlas.objects.get_or_create(breeding_atlas_code=breeding_atlas_code)
-                    bcr, _ = BCR.objects.get_or_create(bcr_code=bcr_code)
-                    usfws, _ = USFWS.objects.get_or_create(usfws_code=usfws_code)
+                    # All of these fields can potentially be blank.
+                    if iba_code == '':
+                        iba = None
+                    else:
+                        iba, _ = IBA.objects.get_or_create(iba_code=iba_code)
+                    if taxonomic_order == '':
+                        tax = None
+                    else:
+                        tax, _ = TaxonomicOrder.objects.get_or_create(taxonomic_order=taxonomic_order)
+                    if species_category == '':
+                        spcat = None
+                    else:
+                        spcat, _ = SpeciesCategory.objects.get_or_create(category=species_category)
+                    if protocol == '':
+                        proto = None
+                    else:
+                        proto, _ = Protocol.objects.get_or_create(protocol_type=protocol)
+                    if project == '':
+                        proj = None
+                    else:
+                        proj, _ = Project.objects.get_or_create(project_code=project)
+                    if breeding_atlas_code == '':
+                        atlas = None
+                    else:
+                        atlas, _ = BreedingAtlas.objects.get_or_create(breeding_atlas_code=breeding_atlas_code)
+                    if bcr_code == '':
+                        bcr = None
+                    else:
+                        bcr, _ = BCR.objects.get_or_create(bcr_code=bcr_code)
+                    if usfws_code == '':
+                        usfws = None
+                    else:
+                        usfws, _ = USFWS.objects.get_or_create(usfws_code=usfws_code)
                     # Continue with models that don't depend on others, but that have multiple attributes.
                     sp, _ = StateProvince.objects.get_or_create(state_province=state_province, defaults={'state_code': state_code})
                     cnty, _ = County.objects.get_or_create(county=county, defaults={'county_code': county_code})
                     local, _ = Locality.objects.get_or_create(locality_id=locality_id, defaults={'locality_type': locality_type, 'locality_name': locality_name})
                     cntry, _ = Country.objects.get_or_create(country=country, defaults={'country_code': country_code})
-                    obs, _ = Observer.objects.get_or_create(observer_id=observer_id, defaults={'first_name': first_name, 'last_name': last_name})
+                    if observer_id == '':
+                        obs = None
+                    else:
+                        obs, _ = Observer.objects.get_or_create(observer_id=observer_id, defaults={'first_name': first_name, 'last_name': last_name})
                     # Then continue with the models that only depend on the ones we've got.
                     loc, _ = Location.objects.get_or_create(coords=coords, defaults={'locality': local, 'country': cntry, 'state_province': sp, 'county': cnty, 'iba_code': iba})
                     sp, _ = Species.objects.get_or_create(scientific_name=scientific_name, defaults={'common_name': common_name, 'taxonomic_order': tax, 'category': spcat})
                     # SubSpecies depends on Species.
-                    subsp, _ = SubSpecies.objects.get_or_create(scientific_name=subspecies_scientific_name, defaults={'parent_species': sp, 'common_name': subspecies_common_name})
+                    if subspecies_scientific_name == '':
+                        subsp = None
+                    else:
+                        subsp, _ = SubSpecies.objects.get_or_create(scientific_name=subspecies_scientific_name, defaults={'parent_species': sp, 'common_name': subspecies_common_name})
                     # Next the checklist model
                     check, _ = Checklist.objects.get_or_create(checklist=checklist_id, defaults={'location': loc, 'start_date_time': start, 'checklist_comments': checklist_comments, 'duration': duration, 'distance': distance, 'area': area, 'number_of_observers': number_of_observers, 'complete_checklist': complete_checklist, 'group_id': group_id, 'approved': approved, 'reviewed': reviewed, 'reason': reason, 'protocol': proto, 'project': proj})
                     # Finally the remaining models that depend on all the previous ones.
                     # We don't care about the result here because get_or_create is being used to be idempotent.
-                    _, _ = Observation.objects.get_or_create(observation=observation_id, defaults={'number_observed': number_observed, 'is_x': is_x, 'age_sex': age_sex, 'species_comments': species_comments, 'species': sp, 'subspecies': subsp, 'breeding_atlas_code': atlas, 'date_last_edit': last_edit, 'has_media': has_media, 'bcr_code': bcr, 'usfws_code': usfws, 'checklist': check})
+                    _, _ = Observation.objects.get_or_create(observation=observation_id, defaults={'number_observed': number_observed, 'is_x': is_x, 'age_sex': age_sex, 'species_comments': species_comments, 'species': sp, 'subspecies': subsp, 'breeding_atlas_code': atlas, 'date_last_edit': last_edit, 'has_media': has_media, 'bcr_code': bcr, 'usfws_code': usfws, 'checklist': check, 'observer': obs})
                 
                     count += 1
                     if count % 1000 == 0:
