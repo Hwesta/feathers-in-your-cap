9aa95256df99156c31044a41058a7a1b43afdeac
Dale Floer <dalefloer@gmail.com>
2017-04-26 17:25:39 -0700

    Added models to admin to be able to inspect data easily.

--- ebird_data/admin.py
+++ ebird_data/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Observation, Observer, BreedingAtlas, Checklist, Protocol, Project, Species, SubSpecies, TaxonomicOrder, SpeciesCategory, Location, Locality, Country, StateProvince, County, IBA, BCR, USFWS
+
+admin.site.register(Observation)
+admin.site.register(Observer)
+admin.site.register(BreedingAtlas)
+admin.site.register(Checklist)
+admin.site.register(Protocol)
+admin.site.register(Project)
+admin.site.register(Species)
+admin.site.register(SubSpecies)
+admin.site.register(TaxonomicOrder)
+admin.site.register(SpeciesCategory)
+admin.site.register(Location)
+admin.site.register(Locality)
+admin.site.register(Country)
+admin.site.register(StateProvince)
+admin.site.register(County)
+admin.site.register(IBA)
+admin.site.register(BCR)
+admin.site.register(USFWS)
